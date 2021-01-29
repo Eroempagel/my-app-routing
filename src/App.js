53876a1370686093c25d0a85032fe445ec811c67
Erik <eroempagel@outlook.com>
2021-01-28 19:14:13 -0600

    added routing and 404 using switch

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import Welcome from "./components/welcome/Welcome";
+import Clock from "./components/clock/Clock";
+import Contact from "./components/contact/Contact";
+import Navigation from "./components/navigation/Navigation";
+import NotFoundPage from "./components/not-found-page/NotFoundPage";
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      <Switch>
+        <Route
+          exact
+          path="/welcome/:name"
+          render={(props) => (
+            <Welcome {...props} name={props.match.params.name} />
+          )}
+        />
+        <Route
+          exact
+          path="/"
+          render={(props) => <Welcome {...props} name="Erik" />}
+        />
+        <Route exact path="/clock" component={Clock} />
+        <Route exact path="/contact" component={Contact} />
+        <Route path="*" component={NotFoundPage} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
